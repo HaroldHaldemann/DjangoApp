f218a3b6f7b2a24dec8f1c2e813fd9a43fe947eb
= <harold.haldemann@gmail.com>
2023-01-21 01:01:33 +0100

    reset tracked files

--- djangoapp/reviews/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-from reviews.models import Review, Ticket, UserFollows
-
-
-admin.site.register(Review)
-admin.site.register(Ticket)
-admin.site.register(UserFollows)
--- djangoapp/reviews/forms.py
+++
@@ -1,5 +0,0 @@
-from django import forms
-
-
-class FollowForm(forms.Form):
-    username = forms.CharField(max_length=63, label="Nom d'utlisateur")
